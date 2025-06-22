87eb5b4142ab593c76972b39b374de241306468e
Owen <64725743+Ownwn@users.noreply.github.com>
2025-06-22 23:11:25 +1200

    Config option for cancelling sprint

--- src/main/java/com/ownwn/RevertOldMovement.java
+++ src/main/java/com/ownwn/RevertOldMovement.java
@@ -1,13 +1,70 @@
 package com.ownwn;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class RevertOldMovement implements ModInitializer {
+import java.io.*;
+import java.util.Map;
 
-	@Override
-	public void onInitialize() {
-	}
+public class RevertOldMovement implements ModInitializer {
+    public static final String MOD_ID = "revert-old-movement";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    // true is default mc behavior, but feels worse
+    public static boolean shouldCancelSprint = true;
+
+    private final File configFile = FabricLoader.getInstance().getConfigDir().resolve("revert-old-movement.json").toFile();
+    private final String configKey = "Cancel Sprint on attack";
+
+    @Override
+    public void onInitialize() {
+        JsonObject config = getConfigJson();
+        if (config == null) {
+            LOGGER.warn("Failed to read json from config file at {}", configFile.getPath());
+            return;
+        }
+
+        try {
+            shouldCancelSprint = config.get(configKey).getAsBoolean();
+        } catch (Exception e) {
+            LOGGER.warn("Failed to read json from config file at {}", configFile.getPath());
+        }
+    }
+
+    public JsonObject getConfigJson() {
+        if (!configFile.exists()) {
+            createDefaultConfig();
+        }
+
+        try {
+            Gson gson = new Gson();
+            JsonReader reader = new JsonReader(new FileReader(configFile));
+            return gson.fromJson(reader, JsonObject.class);
+        } catch (FileNotFoundException ignored) {
+        }
+        return null;
+    }
+
+    public void createDefaultConfig() {
+        try {
+            if (!configFile.createNewFile()) {
+                throw new IOException();
+            }
+            Gson gson = new Gson();
+            FileWriter writer = new FileWriter(configFile);
+
+            gson.toJson(Map.of(configKey, true), writer);
+            writer.close();
+
+
+        } catch (IOException | JsonIOException e) {
+            LOGGER.warn("Failed to create config file at {}", configFile.getPath());
+        }
+    }
 }
---
+++ src/main/java/com/ownwn/mixin/PlayerEntityMixin.java
@@ -0,0 +1,15 @@
+package com.ownwn.mixin;
+
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.ownwn.RevertOldMovement;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
+    @WrapWithCondition(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;setSprinting(Z)V"))
+    private boolean shouldSetSprinting(PlayerEntity instance, boolean sprinting) {
+        return RevertOldMovement.shouldCancelSprint;
+    }
+}
